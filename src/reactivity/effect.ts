4c04f36e47352613f67ddc25c66e3d2054c7c74c
ssaylo <ssaylo@163.com>
2022-05-02 14:55:31 +0800

    feat: effect & reactive & 依赖收集 & 触发依赖

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,47 @@
+
+class ReactiveEffect {
+  private _fn: any;
+
+  constructor(fn: any) {
+    this._fn = fn;
+  }
+
+  run() {
+    activeEffect = this;
+    this._fn();  
+  }
+}
+
+const targetMap = new Map();
+export function track(target: string, key: string | symbol) {
+  //  taget -> key -> dep
+  let depsMap = targetMap.get(target);
+
+  if (!depsMap) { 
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  } 
+  dep.add(activeEffect);
+}
+
+export function trigger(target: string, key: string | symbol) {
+  let depsMap = targetMap.get(target);
+  let dep = depsMap.get(key);
+  for (const effect of dep) {
+    effect.run();
+  }
+}
+
+let activeEffect: any;
+export function effect(fn: any) {
+  // fn
+  const _effect = new ReactiveEffect(fn);
+
+  _effect.run();
+}
--- src/reactivity/tests/index.spec.ts
+++
@@ -1,6 +0,0 @@
-import { add } from "../index"
-
-it("init", () => {
-  expect(add(1, 3)).toBe(4);
-  expect(true).toBe(true);
-})
